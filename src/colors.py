b205e7dceb647762191e3c77d57f963d0cbb1b63
Adrian Orive <adrian.orive.oneca@gmail.com>
2018-11-20 17:28:18 +0100

    Change Color to store as int
    Signed-off-by: Adrian Orive <adrian.orive.oneca@gmail.com>

--- src/colors.py
+++ src/colors.py
@@ -24,78 +24,53 @@ SOFTWARE.
 import colorsys
 
 
+BIT_DEPTH = 255
+
+
 class Color:
     __slots__ = ('_r', '_g', '_b')
 
-    def __init__(self, r: float=0.0, g: float=0.0, b: float=0.0) -> None:
-        # Check boundaries
-        if r < 0.0:
-            r = 0.0
-        elif r > 1.0:
-            r = 1.0
-        if g < 0.0:
-            g = 0.0
-        elif g > 1.0:
-            g = 1.0
-        if b < 0.0:
-            b = 0.0
-        elif b > 1.0:
-            b = 1.0
-
-        # Store the values
-        self._r = r
-        self._g = g
-        self._b = b
+    def __init__(self, r: int=0, g: int=0, b: int=0) -> None:
+        self._r = BIT_DEPTH if r >= BIT_DEPTH else 0 if r <= 0 else r
+        self._g = BIT_DEPTH if g >= BIT_DEPTH else 0 if g <= 0 else g
+        self._b = BIT_DEPTH if b >= BIT_DEPTH else 0 if b <= 0 else b
 
     @classmethod
-    def rgb(cls, r: int=0, g: int=0, b: int=0) -> "Color":
+    def rgb(cls, r: float=0.0, g: float=0.0, b: float=0.0) -> "Color":
         # Check boundaries
-        if r < 0:
-            r = 0
-        elif r > 255:
-            r = 255
-        if g < 0:
-            g = 0
-        elif g > 255:
-            g = 255
-        if b < 0:
-            b = 0
-        elif b > 255:
-            b = 255
+        r = 1.0 if r >= 1.0 else 0.0 if r <= 0.0 else r
+        g = 1.0 if g >= 1.0 else 0.0 if g <= 0.0 else g
+        b = 1.0 if b >= 1.0 else 0.0 if b <= 0.0 else b
 
         # Create the instance
-        return cls(r, g, b)
+        return cls(int(r*BIT_DEPTH), int(g*BIT_DEPTH), int(b*BIT_DEPTH))
 
     @classmethod
     def hls(cls, h: float, l: float, s: float) -> "Color":
         # TODO: boundary check?
-        return cls(*colorsys.hls_to_rgb(h, l, s))
+        return cls.rgb(*colorsys.hls_to_rgb(h, l, s))
 
     @classmethod
     def hsv(cls, h: float, s: float, v: float) -> "Color":
         # TODO: boundary check?
-        return cls(*colorsys.hsv_to_rgb(h, s, v))
+        return cls.rgb(*colorsys.hsv_to_rgb(h, s, v))
 
     @classmethod
     def yiq(cls, y: float, i: float, q: float) -> "Color":
         # TODO: boundary check?
-        return cls(*colorsys.yiq_to_rgb(y, i, q))
+        return cls.rgb(*colorsys.yiq_to_rgb(y, i, q))
 
     def __str__(self) -> str:
-        return "RGB({}, {}, {})".format(
-            int(self._r * 255),
-            int(self._g * 255),
-            int(self._b * 255),
-        )
+        return "RGB({}, {}, {})".format(self._r, self._g, self._b)
 
     @property
-    def r(self) -> float:
+    def r(self) -> int:
         return self._r
 
     @property
-    def g(self) -> float:
+    def g(self) -> int:
         return self._g
 
     @property
-    def b(self) -> float:
+    def b(self) -> int:
         return self._b
