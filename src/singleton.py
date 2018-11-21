f96a46bda3263790708385a517e0897fa9fc0dd3
Adrian Orive <adrian.orive.oneca@gmail.com>
2018-11-21 14:57:07 +0100

    Change Singleton patter to a lazy approach
    Signed-off-by: Adrian Orive <adrian.orive.oneca@gmail.com>

--- src/singleton.py
+++ src/singleton.py
@@ -39,7 +39,7 @@ def mangle(class_name: str, attribute: str) -> str:
 
 class Singleton(type):
     """
-    Metaclass that turns the class into a singleton
+    Metaclass that turns the class into a singleton lazily initialized
 
     It uses a class variable to store the single instance which is created at
     class creation by calling its constructor with no arguments. This means
@@ -47,41 +47,92 @@ class Singleton(type):
     parameters. However, singletons should not need initialization parameters
     as they only have one instance.
 
-    The private class attribute __instance is used internally. If this wants to
-    be used by the class, another attribute name should be provided through the
-    instance class keyword argument.
+    The private class attributes __instance and __new are used internally.
+    If they want to be used by the class, another attribute name should be
+    provided through the `instance` or `constructor` keyword argument of the
+    class respectively.
 
     Usage:
         class SingleExample(metaclass=Singleton):
             def __init__(self):
                 pass
 
-        class SingleExample2(metaclass=Singleton, instance='__qwerty'):
+        class SingleExample2(metaclass=Singleton, constructor='__qwerty'):
+            __new = "my class variable"
+            def __init__(self):
+                pass
+
+        class SingleExample3(metaclass=Singleton, instance='__asdfg'):
+            __instance = "my class variable"
+            def __init__(self):
+                pass
+
+        class SingleExample4(metaclass=Singleton, constructor='__qwerty',
+                             instance='__asdfg'):
+            __new = "my class variable"
             __instance = "my class variable"
             def __init__(self):
                 pass
     """
+    def __new__(
+            mcs,
+            name: str,
+            bases: Tuple,
+            namespace: Dict[str, Any],
+            constructor: str='__new',
+            **kwargs,
+    ) -> type:
+        # Name mangling
+        mangled_constructor = mangle(name, constructor)
+
+        # Panic if constructor attribute is already used
+        if mangled_constructor in namespace:
+            raise AttributeError(
+                "Singleton pattern uses {}.{} but it was already defined in "
+                "the class".format(name, constructor))
+
+        # Store the previous constructor if exists
+        if '__new__' in namespace:
+            namespace[mangled_constructor] = namespace['__new__']
+            del namespace['__new__']
+
+        return super().__new__(mcs, name, bases, namespace)
+
     def __init__(
             cls,
             name: str,
             bases: Tuple,
-            attrs: Dict[str, Any],
+            namespace: Dict[str, Any],
+            constructor: str='__new',
             instance: str='__instance',
     ) -> None:
         # Name mangling
+        mangled_constructor = mangle(name, constructor)
         mangled_instance = mangle(name, instance)
 
-        # Panic if it is already used
-        if mangled_instance in attrs:
+        # Panic if instance attribute is already used
+        if mangled_instance in namespace:
             raise AttributeError(
                 "Singleton pattern uses {}.{} but it was already defined in "
-                "the class".format(name, instance)
-            )
+                "the class".format(name, instance))
 
-        # Create the single instance and store it
-        setattr(cls, mangled_instance, cls())
+        # Extract the constructor method as we are going to replace it
+        new = namespace.pop(mangled_constructor, cls.__new__)
 
-        # Replace the constructor method
-        cls.__new__ = lambda kls: getattr(kls, mangled_instance)
+        # Method to return the pre-created singleton instance
+        def get_instance(kls):
+            return getattr(kls, mangled_instance)
 
-        super().__init__(name, bases, attrs)
+        # Wrapped constructor method
+        def wrapped_new(kls):
+            # Create the single instance and store it
+            setattr(kls, mangled_instance, new(kls))
+            # Replace the constructor method and execute it
+            kls.__new__ = get_instance
+            return kls.__new__(kls)
+
+        # Follow the init chain
+        super().__init__(name, bases, namespace)
+
+        # Assign the first time constructor to the class
+        cls.__new__ = wrapped_new
